baaa6c7a71bcb9a91979305b4710dc082039b0eb
julioomoura <juliomourajom@gmail.com>
2020-03-22 01:53:11 -0300

    :cool: Change docker install to beginning

--- script.sh
+++ script.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # Pacotes apt
-apt_packages=(vim git curl zip unzip snapd snapd-xdg-open ubuntu-restricted-extras gparted)
+apt_packages=(vim git curl zip unzip snapd snapd-xdg-open ubuntu-restricted-extras gparted gnome-tweak-tool apt-transport-https ca-certificates curl software-properties-common)
+
+sudo apt update && sudo apt dist-upgrade -y
+sudo apt autoclean
+sudo apt autoremove -y
 
 for p in "${apt_packages[@]}"
 do
  sudo apt install $p -y
 done
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
 
 # Chrome
 
@@ -17,32 +28,22 @@ sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 
 # SDKMan e Java 8 
 curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 8.0.232.j9-adpt 
+source $HOME/.sdkman/bin/sdkman-init.sh
+sdk install java 8.0.242.hs-adpt
 
 # Node 12
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 
-# Snap 
+# Snap
 sudo snap install --classic vscode
 sudo snap install --classic slack
 sudo snap install spotify
 sudo snap install discord
 sudo snap install vlc
-sudo snap install zoom
 sudo snap install postman
 
 
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
 sudo apt autoremove -y
-
-# Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
-su - ${USER}
