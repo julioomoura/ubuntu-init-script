9b5fa4c99c1b9f0855bd24eee0211d8a5d3ec45e
Júlio Moura <37306903+julioomoura@users.noreply.github.com>
2020-03-23 00:23:23 -0300

    :bug: Confirm docker-ce installation

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ done
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 sudo setfacl -m user:$USER:rw /var/run/docker.sock
 
