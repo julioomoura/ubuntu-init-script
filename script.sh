6152a797cb53356b8ec339bb373d810dd4d564a4
Júlio Moura <37306903+julioomoura@users.noreply.github.com>
2021-01-17 20:09:40 -0300

    :recycle: Update script.sh

--- script.sh
+++ script.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # Pacotes apt
-apt_packages=(vim git curl zip unzip snapd snapd-xdg-open ubuntu-restricted-extras gparted gnome-tweak-tool apt-transport-https ca-certificates software-properties-common tmux flameshot)
+apt_packages=(vim git curl zip unzip docker-ce nodejs snapd snapd-xdg-open ubuntu-restricted-extras gparted gnome-tweak-tool apt-transport-https ca-certificates software-properties-common tmux flameshot)
 
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
 sudo apt autoremove -y
+
+# Docker Repository 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Node 12
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 for p in "${apt_packages[@]}"
 do
  sudo apt install $p -y
 done
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 sudo setfacl -m user:$USER:rw /var/run/docker.sock
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 # Chrome
-
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 DIRETORIO_DOWNLOADS="$HOME/Downloads/"
 wget -c "$URL_GOOGLE_CHROME" -P "$DIRETORIO_DOWNLOADS"
@@ -35,18 +38,13 @@ source $HOME/.sdkman/bin/sdkman-init.sh
 sdk install java 8.0.275.hs-adpt
 sdk install maven
 
-# Node 12
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y nodejs
-
 # Snap
-sudo snap install --classic code
-sudo snap install --classic slack
-sudo snap install spotify
-sudo snap install discord
-sudo snap install vlc
-sudo snap install postman
+snap_packages=("--classic code" "--classic slack" spotify discord vlc postman dbeaver-ce)
 
+for p in "${apt_packages[@]}"
+do
+ sudo snap install $p
+done
 
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
