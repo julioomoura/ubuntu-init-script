95457c442a0afad339300d3969adeb9b7b60b2c7
Julio <juliomourajom@gmail.com>
2022-03-24 20:30:32 -0300

    Adding info logs

--- script.sh
+++ script.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# Consts
 DOWNLOADS_DIRECTORY="$HOME/Downloads/"
+GREEN='\e[1;92m'
+COLORLESS='\e[0m'
+
 
 # Pacotes apt
 apt_packages=(
     vim git curl zip unzip gparted gnome-tweak-tool flameshot flatpak apt-transport-https
-    ca-certificates gnupg lsb-release
+    ca-certificates gnupg lsb-release wget
 )
 
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
 sudo apt autoremove -y
+
+echo -e "${GREEN}[INFO] - Installing apt packages${COLORLESS}"
 
 for p in "${apt_packages[@]}"
 do
@@ -32,6 +38,8 @@ urls=(
     ["insomnia"]="https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app"
 )
 
+echo -e "${GREEN}[INFO] - Installing .deb packages${COLORLESS}"
+
 for u in "${!urls[@]}"
 do
   wget -c ${urls[$u]} -P "$DOWNLOADS_DIRECTORY" -O "${u}.deb"
@@ -39,18 +47,23 @@ done
 sudo dpkg -i $DOWNLOADS_DIRECTORY/*.deb
 
 # NVM
+echo -e "${GREEN}[INFO] - Installing nvm${COLORLESS}"
+
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install --lts  
 
 # SDKMan
+echo -e "${GREEN}[INFO] - Installing sdkman${COLORLESS}"
+
 curl -s "https://get.sdkman.io" | bash
 source $HOME/.sdkman/bin/sdkman-init.sh
 sdk install java
 sdk install maven
 
 # Flatpak
-
 flatpak_packages=("com.spotify.Client" "org.videolan.VLC" " com.getpostman.Postman" "com.obsproject.Studio")
+
+echo -e "${GREEN}[INFO] - Installing flatpak packages${COLORLESS}"
 
 for p in "${flatpak_packages[@]}"
 do
@@ -71,13 +84,18 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 # Change git init default branch name to main
 git config --global init.defaultBranch main
 
+
+echo -e "${GREEN}[INFO] - Installing brave${COLORLESS}"
 # Brave browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
+echo -e "${GREEN}[INFO] - Installing docker${COLORLESS}"
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
+
+  echo -e "${VERDE}[INFO] - FINISH${SEM_COR}"
